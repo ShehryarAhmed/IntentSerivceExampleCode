86868f2cc1375150dc5a51db9b8cc03741f2a2c4
ShehryarAhmedKhan <shehryar.khan@cloudasset.com>
2021-11-16 16:20:33 +0500

    Intent Service Example

---
+++ app/src/main/java/com/example/serviceexample/DownloadService.java
@@ -0,0 +1,90 @@
+package com.example.serviceexample;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DownloadService extends IntentService {
+
+    private int result = Activity.RESULT_CANCELED;
+    public static final String URL = "urlpath";
+    public static final String FILENAME = "filename";
+    public static final String FILEPATH = "filepath";
+    public static final String RESULT = "result";
+    public static final String NOTIFICATION = "com.example.serviceexample";
+
+    public DownloadService() {
+        super("DownloadService");
+    }
+
+    // will be called asynchronously by Android
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("LogonHandleIntent","onHandleIntent");
+        String urlPath = intent.getStringExtra(URL);
+        String fileName = intent.getStringExtra(FILENAME);
+        File output = new File(Environment.getExternalStorageDirectory(),
+                fileName);
+        if (output.exists()) {
+            output.delete();
+        }
+
+        InputStream stream = null;
+        FileOutputStream fos = null;
+        try {
+
+            URL url = new URL(urlPath);
+            stream = url.openConnection().getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            fos = new FileOutputStream(output.getPath());
+            int next = -1;
+            while ((next = reader.read()) != -1) {
+                fos.write(next);
+            }
+            // successfully finished
+            result = Activity.RESULT_OK;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        publishResults(output.getAbsolutePath(), result);
+    }
+
+    private void publishResults(String outputPath, int result) {
+        Log.d("LogpublishResults","publishResults");
+        Toast.makeText(getApplicationContext(), "Testing...", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(NOTIFICATION);
+        intent.putExtra(FILEPATH, outputPath);
+        intent.putExtra(RESULT, result);
+        sendBroadcast(intent);
+    }
+}
